ac456a6c05cebbf0ad7a6f821f1386fc8f860975
JeehongKim0407 <143788596+JeehongKim0407@users.noreply.github.com>
2025-07-16 06:11:02 +0000

    Deploying to gh-pages from @ JeehongKim0407/JeehongKim0407.github.io@f1848baed8fef3d279b83910c1f972f949b6c189 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -50,16 +50,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-joined-viba-at-seoul-national-university",
+          title: 'I joined ViBA at Seoul National University.',
+          description: "",
+          section: "News",},{id: "news-a-paper-got-accepted-at-aaai-2025-workshop-on-anomaly-detection-in-scientific-domains",
+          title: 'A paper got accepted at AAAI 2025 Workshop on Anomaly Detection in Scientific...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
